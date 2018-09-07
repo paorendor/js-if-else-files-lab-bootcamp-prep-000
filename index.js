12c8bb65a0b8530bd7f43f0ebf57f31743961f0c
paorendor@gmail.com rendor <paorendor@gmail.com>
2018-09-07 22:47:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var name = "Susan"
 var height = "70"
 
-if (name === "Susan")
+if (name === "Susan" && height === 70)
 {
   console.log("The name is correct!")
+  console.log("The height is correct!")
 }
 else
 {
   console.log ("Expected the name to be Susan")
+  console.log("Expected the height to be 70")
 }
 
 
