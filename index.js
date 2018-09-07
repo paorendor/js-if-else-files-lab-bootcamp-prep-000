7d1d283800958f35bee19192ebb7dc950fb2010a
paorendor@gmail.com rendor <paorendor@gmail.com>
2018-09-07 22:45:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var name = "Susan"
 var height = "74"
 
-if (name === "Susan" && height === 70)
+if (name === "Susan")
 {
   console.log("The name is correct!")
   console.log("The height is correct!")
