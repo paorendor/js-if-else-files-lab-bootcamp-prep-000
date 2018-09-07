b58debe49304458f01cd7474604a778df0919ebf
paorendor@gmail.com rendor <paorendor@gmail.com>
2018-09-07 22:46:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,14 +4,20 @@ var height = "74"
 if (name === "Susan")
 {
   console.log("The name is correct!")
-  console.log("The height is correct!")
 }
 else
 {
   console.log ("Expected the name to be Susan")
 }
 
-
+if (height === 70)
+{
+  console.log("The height is correct!")
+}
+else
+{
+  console.log ("Expected height to be is 74")
+}
 // Don't worry about this
 module.exports = { name, height
 }
